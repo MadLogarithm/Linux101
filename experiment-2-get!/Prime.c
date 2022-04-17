99df11a6c71284e575486fcbdb1e2518a4c6efc3
MadLogarithm <74230760+MadLogarithm@users.noreply.github.com>
2022-04-17 22:24:35 +0800

    Add files via upload

--- experiment-2-get!/Prime.c
+++ experiment-2-get!/Prime.c
@@ -2,10 +2,9 @@
 #include<math.h>
 #include"include/Prime.h"
 void IsPrime(int x){
-	int k=sqrt(x);
     int t=0;
     if(x%2==0&&x!=2)t++; 
-	for(int i=3;i<=k;i+=2) if(x%i==0)t++;
+	for(int i=3;i<=x;i+=2) if(x%i==0)t++;
 	if(t>0)printf("%d isn't' Prime.\n",x); 
 	else printf("%d is Prime.\n",x);
 	return;
